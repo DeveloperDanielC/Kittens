992ffc3b5f2360846b317b00c58082de1e785c03
DeveloperDanielC <32421181+DeveloperDanielC@users.noreply.github.com>
2017-10-08 14:06:32 -0400

    First Commit

---
+++ SaveTheKittens2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Daniel Claggett on 10/8/17.
+//  Copyright © 2017 Daniel Claggett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Kittens")
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
